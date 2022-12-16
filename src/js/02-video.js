8652cb653a09522be958ab258d6bf14b6ebd4e79
PPacuk <piotrek.pacuk@gmail.com>
2022-12-16 09:56:23 +0100

    02-08 done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,32 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const LOCALSTORAGE = 'videoplayer-current-time';
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.on(
+  'timeupdate',
+  throttle(function (time) {
+    localStorage.setItem(LOCALSTORAGE, JSON.stringify(time.seconds));
+  }, 1000)
+);
+
+player
+  .setCurrentTime(localStorage.getItem(LOCALSTORAGE))
+  .then(function (seconds) {})
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        break;
+
+      default:
+        break;
+    }
+  });
+
+player.on('ended', function (time) {
+  setTimeout(() => {
+    localStorage.setItem(LOCALSTORAGE, '0.0');
+  }, 1000);
+});
